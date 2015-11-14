ec6d25f8e0174a71a2e32eda1db3bbe2434225ce
Alex Barry <outerpasta@gmail.com>
2015-11-13 19:38:33 -0800

    Move inline javascript pt.2

--- js/getLinkColor.js
+++ js/getLinkColor.js
@@ -1,1 +1,3 @@
-var elem = document.evaluate("/html/body/div/header/div/div[3]/a/span",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;var color = window.getComputedStyle(elem, null).getPropertyValue("color");return color;
+var elem = document.evaluate("/html/body/div/header/div/div[3]/a/span", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+var color = window.getComputedStyle(elem, null).getPropertyValue("color");
+return color;
